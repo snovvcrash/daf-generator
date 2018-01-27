1a196b9c88769007567d158d27619126e8c74bdc
Sam Freeside <snovvcrash@users.noreply.github.com>
2018-01-27 18:49:26 +0300

    Update ui_dafgen.py

--- ui_dafgen.py
+++ ui_dafgen.py
@@ -6,37 +6,12 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-"""
-@file ui_dafgen.py
-@author snovvcrash <snovvcrash@protonmail.com>
-@date 2018-01
-
-@brief Simple DAF Generator Ui
-
-@license
-Copyright (C) 2018 snovvcrash
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-@endlicense
-"""
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_DAFGen(object):
     def setupUi(self, DAFGen):
         DAFGen.setObjectName("DAFGen")
-        DAFGen.resize(441, 95)
+        DAFGen.resize(451, 95)
         font = QtGui.QFont()
         font.setPointSize(11)
         DAFGen.setFont(font)
@@ -53,27 +28,27 @@ class Ui_DAFGen(object):
         self.actualDelayLabel.setFont(font)
         self.actualDelayLabel.setObjectName("actualDelayLabel")
         self.startButton = QtWidgets.QPushButton(DAFGen)
-        self.startButton.setGeometry(QtCore.QRect(190, 50, 71, 31))
+        self.startButton.setGeometry(QtCore.QRect(200, 50, 71, 31))
         self.startButton.setObjectName("startButton")
         self.stopButton = QtWidgets.QPushButton(DAFGen)
-        self.stopButton.setGeometry(QtCore.QRect(270, 50, 71, 31))
+        self.stopButton.setGeometry(QtCore.QRect(280, 50, 71, 31))
         self.stopButton.setObjectName("stopButton")
         self.quitButton = QtWidgets.QPushButton(DAFGen)
-        self.quitButton.setGeometry(QtCore.QRect(350, 50, 71, 31))
+        self.quitButton.setGeometry(QtCore.QRect(360, 50, 71, 31))
         self.quitButton.setObjectName("quitButton")
         self.delaySlider = QtWidgets.QSlider(DAFGen)
-        self.delaySlider.setGeometry(QtCore.QRect(110, 10, 241, 21))
+        self.delaySlider.setGeometry(QtCore.QRect(120, 10, 241, 21))
         self.delaySlider.setMinimum(50)
         self.delaySlider.setMaximum(200)
         self.delaySlider.setOrientation(QtCore.Qt.Horizontal)
         self.delaySlider.setObjectName("delaySlider")
         self.delayEdit = QtWidgets.QPlainTextEdit(DAFGen)
-        self.delayEdit.setGeometry(QtCore.QRect(360, 10, 61, 31))
+        self.delayEdit.setGeometry(QtCore.QRect(370, 10, 61, 31))
         self.delayEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.delayEdit.setReadOnly(True)
         self.delayEdit.setObjectName("delayEdit")
         self.actualDelayEdit = QtWidgets.QPlainTextEdit(DAFGen)
-        self.actualDelayEdit.setGeometry(QtCore.QRect(110, 50, 61, 31))
+        self.actualDelayEdit.setGeometry(QtCore.QRect(120, 50, 61, 31))
         self.actualDelayEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.actualDelayEdit.setReadOnly(True)
         self.actualDelayEdit.setObjectName("actualDelayEdit")
